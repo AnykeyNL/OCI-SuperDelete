3c428fa2afe0491ac3f6745f956a0bb9041f2311
Richard Garsthagen <the.anykey@gmail.com>
2022-01-26 14:25:40 +0100

    Switching for generic service to a universal module to delete the object. First tests in the update with the deletion if instances

--- ocimodules/kms.py
+++ ocimodules/kms.py
@@ -25,7 +25,7 @@ def DeleteKMSvaults(config, Compartments, MovetoCompartmentID):
         for secret in secrets:
             secretchangedetails = oci.vault.models.ChangeSecretCompartmentDetails()
             secretchangedetails.compartment_id = MovetoCompartmentID
-            print("Moving secret {} to main/trash compartment".format(secret.display_name))
+            print("Moving secret {} to main/trash compartment".format(secret.secret_name))
             vaultClient.change_secret_compartment(secret_id=secret.id, change_secret_compartment_details=secretchangedetails, retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY)
 
     for item in AllItems:
