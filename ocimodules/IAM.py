216b0eacd98898c868021a86101d733ba141aa8c
Richard Garsthagen <the.anykey@gmail.com>
2022-02-09 11:50:35 +0100

    Big overhaul! No more individual modules per service. The AnyDelete can delete most services. Added support for instance principle and delegation token (cloud shell) authentication

--- ocimodules/IAM.py
+++ ocimodules/IAM.py
@@ -34,10 +34,12 @@ def GetCompartments(identity, rootID):
 #################################################
 def Login(config, signer, startcomp):
     identity = oci.identity.IdentityClient(config, signer=signer)
-    print ("debug:")
-    print (config)
-    user = identity.get_user(config["user"]).data
-    print("Logged in as: {} @ {}".format(user.description, config["region"]))
+    if "user" in config:
+        user = identity.get_user(config["user"]).data
+        print("Logged in as: {} @ {}".format(user.description, config["region"]))
+    else:
+        print("Logged in as: {} @ {}".format("InstancePrinciple/DelegationToken", config["region"]))
+        user = "IP-DT"
 
     c = []
 
